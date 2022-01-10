a5026c5ab076a340d1b803757a9e449b0ecf3cc2
huangjianda <huangjianda@deepexi.com>
2022-01-10 19:34:43 +0800

    ✨ feat(offer_one): 数组中出现次数超过—半的数字

---
+++ offer_demo/MoreThanHalfNum.py
@@ -0,0 +1,27 @@
+'''
+@filename		:MoreThanHalfNum.py
+@Description	:数组中出现次数超过—半的数字
+@Date			:2022/01/10 17:26:19
+@Author      	:hjd
+@version      	:1.0
+'''
+
+
+def more_half_num(numbers):
+    number, count = numbers[0], 1
+
+    for n in numbers[1:]:
+        if count == 0:
+            number = n
+            count = 1
+        if n != number:
+            count -= 1
+        else:
+            count += 1
+    
+    return number
+
+if __name__ == "__main__":
+    numbers = [1, 2, 3, 4, 4, 4, 4, 4, 5, 4, 6, 4]
+
+    print(more_half_num(numbers))
