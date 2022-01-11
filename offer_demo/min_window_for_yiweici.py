555791c6da5bc7402a88e9fdea42b599bcfa5a55
huangjianda <huangjianda@deepexi.com>
2022-01-11 14:35:29 +0800

    ✨ feat(min_window): 增加滑动窗口识别字母异位词

---
+++ offer_demo/min_window_for_yiweici.py
@@ -0,0 +1,45 @@
+'''
+@filename		:min_window_for_yiweici.py
+@Description	:找到字符串中所有字母异位词
+@Date			:2022/01/11 14:20:39
+@Author      	:hjd
+@version      	:1.0
+'''
+
+def min_window(s, t):
+    result = []
+    left, right = 0, 0
+    distance = len(t)
+    t_fre = {}
+    for tt in t:
+        t_fre.setdefault(tt, 0)
+        t_fre[tt] += 1
+
+    while right < len(s):
+        
+        if s[right] in t_fre.keys():
+            if t_fre[s[right]] > 0:
+                distance -= 1
+            t_fre[s[right]] -= 1
+
+        right += 1
+
+        while distance == 0:
+            if right - left == len(t):
+                result.append(left)
+
+            if s[left] in t_fre.keys():
+                if t_fre[s[left]] == 0:
+                    distance += 1
+                
+                t_fre[s[left]] += 1
+            
+            left += 1
+    return result
+
+
+if __name__ == "__main__":
+    s = "cbaebabacd"
+    t = "abc"
+
+    print(min_window(s, t))
