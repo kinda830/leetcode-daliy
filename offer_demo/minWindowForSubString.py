e6b1f9270007e78e18e134f171e229cf319f6439
huangjianda <huangjianda@deepexi.com>
2022-01-11 14:11:08 +0800

    🐞 fix(min_windows_sub_string): 优化dict的键值判断

--- offer_demo/minWindowForSubString.py
+++ offer_demo/minWindowForSubString.py
@@ -6,50 +6,47 @@
 @version     :1.0
 '''
 
-
 def min_window(s, t):
     result = ""
 
     if s and t:
-        t_fre = {}
-        for i in t:
-            t_fre.setdefault(i, 0)
-            t_fre[i] += 1
+        t_fre ={}
+        for tt in t:
+            t_fre.setdefault(tt, 0)
+            t_fre[tt] += 1
+
+        begin, left, right = 0, 0, 0
 
         distance = len(t)
-        min_length = len(s) + 1
+        min_lenght = len(s) + 1
 
-        begin = 0
-
-        left = 0
-        right = 0
         while right < len(s):
-            char_right = s[right]
-            if t_fre[char_right] > 0:
-                distance -= 1
+            
+            if s[right] in t_fre.keys():
+                if t_fre[s[right]] > 0:
+                    distance -= 1
 
-            t_fre[char_right] -= 1
+                t_fre[s[right]] -= 1
             right += 1
 
             while distance == 0:
-                if right - left < min_length:
-                    min_length = right - left
+                if right - left < min_lenght:
+                    min_lenght = right - left
                     begin = left
+                if s[left] in t_fre.keys():
+                    if t_fre[s[left]] == 0:
+                        distance += 1
 
-                char_left = s[left]
-                if t_fre[char_left] == 0:
-                    distance += 1
-
-                t_fre[char_left] += 1
+                    t_fre[s[left]] += 1
                 left += 1
-
-        if min_length < len(s) + 1:
-            result = s[begin:begin + min_length]
+                
+        if min_lenght < len(s) + 1:
+            result = s[begin:begin+min_lenght]
 
     return result
 
 
 if __name__ == "__main__":
-    s = "ABAACBAB"
+    s = "ABTAACBAB"
     t = "ABC"
     print(min_window(s, t))
